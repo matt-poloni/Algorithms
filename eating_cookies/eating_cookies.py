30a10ccbe4cab0a368c246e707eb1caf2e30c2d9
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-11-07 00:04:54 -0500

    Finally finishes eating_cookies (w/o factorials)

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,40 +2,38 @@
 
 import sys
 
-def factorial(n, cache=[]):
-    # Try to return a cached value
-    try:
-        result = cache[n]
-    # If a cached value doesn't exist...
-    except:
-        if len(cache) == 0:
-            cache.extend([1, 1])
-        # Generate new factorials from the largest cached
-        if n >= 2:
-            for i in range(len(cache), n+1):
-                # And cache each new factorial along the way
-                cache.append(i * cache[i-1])
-        result = cache[n]
-    return result
+# def factorial(n, cache=[]):
+#     # Try to return a cached value
+#     try:
+#         result = cache[n]
+#     # If a cached value doesn't exist...
+#     except:
+#         if len(cache) == 0:
+#             cache.extend([1, 1])
+#         # Generate new factorials from the largest cached
+#         if n >= 2:
+#             for i in range(len(cache), n+1):
+#                 # And cache each new factorial along the way
+#                 cache.append(i * cache[i-1])
+#         result = cache[n]
+#     return result
 
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
 
-def eating_cookies(n, cache=[]):
+def eating_cookies(n, _=None, cache=[]):
     # Try to return a cached value
     try:
         result = cache[n]
     except:
         if len(cache) == 0:
-            cache.extend([1, 1, 2, 4])
-        if n >= 4:
-            # 
+            cache.extend([1, 1, 2])
+        if n >= 3:
+            val = eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
+            cache.append(val)
         result = cache[n]
     return result
-        
-        
-    
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
