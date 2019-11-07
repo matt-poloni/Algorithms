83f082f20d973dcebf91d41cf51ae17b051314e8
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-11-06 19:13:07 -0500

    Sets up a factorial helper for eating_cookies, knowing they'll be needed

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,12 +5,40 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+def factorial(n, cache=[]):
+    # Try to return a cached value
+    try:
+        result = cache[n]
+    # If a cached value doesn't exist...
+    except:
+        if len(cache) == 0:
+            cache.extend([1, 1])
+        # Generate new factorials from the largest cached
+        for i in range(len(cache), n+1):
+            # And cache each new factorial along the way
+            cache.append(i * cache[i-1])
+        result = cache[n]
+    return result
+
+def eating_cookies(n, cache={}):
+    try:
+        return cache[n]
+    except:
+        if n < 0:
+            print("ERROR: No negative numbers allowed")
+        elif n < 4:
+            cache[0] = 1
+            cache[1] = 1
+            cache[2] = 2
+            cache[3] = 4
+    return cache[n]
+        
+        
+    
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+    if len(sys.argv) > 1:
+        num_cookies = int(sys.argv[1])
+        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+    else:
+        print('Usage: eating_cookies.py [num_cookies]')
